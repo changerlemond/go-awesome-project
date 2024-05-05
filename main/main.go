59db366ac766b76576a0df4faf0e12c3312ca3e4
changerlemond <83348294+changerlemond@users.noreply.github.com>
2024-05-05 18:31:59 +0900

    feature: gin framework setting and test

--- main/main.go
+++ main/main.go
@@ -1,47 +1,21 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
-	var db *sql.DB
-	var err error
-	var raw *user_raw
-
-	db, err = connectDatabase()
-
-	if err != nil {
-		panic("데이터베이스가 연결되지 않았습니다.")
-	}
-
-	fmt.Println("DB Ready.")
-
-	_, err = createUsersTable(db)
-
-	if err != nil {
-		panic("유저 테이블이 생성되지 않았습니다.")
-	}
-
-	fmt.Println("Table Created.")
-
-	_, err = insertUser(db, "abc@example.com", "12345678")
-
-	if err != nil {
-		fmt.Println(err)
-		panic("유저가 생성되지 않았습니다.")
-	}
-
-	fmt.Println("User Created.")
-
-	raw, err = getUser(db)
-
-	if err != nil {
-		panic("유저를 불러오는데 실패했습니다.")
-	}
-	fmt.Println()
-	fmt.Println("user_id", raw.user_id)
-	fmt.Println("email", raw.email)
-	fmt.Println("password", raw.password)
+	r := gin.Default() // default setting
+	// 1. String data
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World") // http status, response data
+	}) // url, handler function
+	// 2. JSON data
+	r.GET("/json", func(c *gin.Context) {
+		c.JSONP(http.StatusOK, gin.H{
+			"message": "Hello World",
+		}) // gin.H{}: map you can configure
+	}) // url, handler function
+	r.Run("localhost:8080") // api running url, port
 }

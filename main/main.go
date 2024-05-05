cfb5bb462cb691b718125bcd32a5e0dde904b98c
changerlemond <83348294+changerlemond@users.noreply.github.com>
2024-05-05 17:27:09 +0900

    test: html form, json

--- main/main.go
+++ main/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
 )
 
+type ContactDetails struct {
+	Email   string
+	Subject string
+	Message string
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		_, _ = fmt.Fprintf(w, "Hello, World!")
-	})
-	_ = http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+	tmpl := template.Must(template.ParseFiles("form.html"))
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_ = tmpl.Execute(w, nil)
+	}).Methods("GET")
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var details ContactDetails
+		_ = json.NewDecoder(r.Body).Decode(&details) // read body, then decode
+		_ = json.NewEncoder(w).Encode(details)       // encode, then send to user
+	}).Methods("POST")
+
+	_ = http.ListenAndServe(":8080", r)
 }
